153ee2c03abba79d197c8d4fb9245e527540cb4d
Ali_Rahmani <ali93rahmani@gmail.com>
2021-08-20 20:05:45 +0430

    cart model buFix

--- BabaBarghi/BabaBarghi/settings.py
+++ BabaBarghi/BabaBarghi/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
     # "django_extensions",
 
-    "product",
+    "product", "cart",
     # "user",
 ]
 
--- BabaBarghi/cart/models.py
+++ BabaBarghi/cart/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from product.models import Product
 
-
 # Create your models here.
 
 class Cart(models.Model):
     count = models.IntegerField(verbose_name="تعداد محصول")
     user_fk = None
-    prodct_fk = models.ManytoManyField("Product", on_delete=models.CASCADE)
+    prodct_fk = models.ForeignKey("product.Product", on_delete=models.CASCADE, default=None)
 
     # if payment_check == "OK" then add to User__score
-    score_of_this_cart = models.SmallAutoField(help_text="امتیاز این سبد خرید را در خودش نگه میداره ")
+    score_of_this_cart = models.PositiveSmallIntegerField(default=0, help_text="امتیاز این سبد خرید را در خودش نگه میداره ")
