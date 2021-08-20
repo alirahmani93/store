a5783d501b7d9b2386dcec45d51f54610e8e8cb4
Ali_Rahmani <ali93rahmani@gmail.com>
2021-08-20 19:46:46 +0430

    cart admin

--- BabaBarghi/cart/admin.py
+++ BabaBarghi/cart/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Cart
 # Register your models here.
+
+@admin.register(Cart)
+class BrandAdmin(admin.ModelAdmin):
+    pass
+
+#########_____________________###########
+# class ProductInline(admin.TabularInline):
+#     model = Product
+#     extra = 2
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     inlines = [ProductInline]
+
+#### Use ""modelAdmin.fiedset"" in your admin #########
